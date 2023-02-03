f4ab081550769f5a252839b9dff59ebed12e404d
Anshit Bakshi <bakshianshit21@gmail.com>
2023-02-03 19:08:09 +0530

    Adding all the files for CS50s Final Project

---
+++ README.md
@@ -0,0 +1,67 @@
+# Discipliner
+#### Video Demo:  <https://youtu.be/ARC2bJx9PUU>
+#### Description: The three main parts of my project are described below.
+#### Password Manager:
+This is a simple password manager to store all your passwords in a sqlite database and with one master password you can look at all your passwords and you don't have to worry about forgeting a password ever again.
+
+To ensure proper safety and prevent your passwords from getting hacked the passwords stored in the database are encrypted using Fernet which provides symmetric encryption. Hence , none of your real passwords are stored on the database rather the encrypted form is stored which is decrypted again when to show you when you request the Password Table/
+
+### Task Manager:
+Ever feel like you're loosing track of all the tasks you want to do , Discipliner has a task manager section where you can easily list all your tasks and give them a particular deadline and upon completion of the task you can just tick the checkbox on the right most column. It'll help you get organized and be on track for all your tasks.
+
+Task manager keeps you organized and on track you can create tasks and assign a specific deadline to those tasks , once you're finished doing that task you can simply check the completed box.
+
+
+### Notes Manager:
+How many times have you forgotten important info when you needed it the most , the notes section of Discipliner let's you keep track of all that information by putting it in a table.
+
+The notes manager automatically assigns a date to the content added so that you exactly remember when you added that note and you do not forget the context of the note.
+Every note has a title associated with it so that it is easier for you to find notes of specific titles without going through the content of each note.
+
+
+### Database Explained :
+To store data sqlite database was used which consisted of tables :
+
+### Users :
+The users table stored the user_id ,username , master password for every user and upon a login request the password provided by the user was checked against the username and password in this table.
+
+To ensure proper security measures the master password is stored in an encrypted manner using the encryption provided by werkzeug.security
+
+
+### Notes :
+ Thi
+
+#### Technologies Used :
+ * Python
+ * Flask
+ * HTML
+ * CSS
+ * Bootstrap
+ * Fernet
+
+
+#### How to Run:
+* Download the Source Code
+* Locate app.Py
+* Run pip install requirements.txt
+* Run flask run
+* (Optional) Run phpliteadmin database.db to check the database and see the tables
+
+
+#### Documentation :
+ * Flask - <https://flask.palletspro jects.com/en/2.2.x/>
+ * Python - <https://docs.python.org/3/>
+ * Sqlite3 - <https://www.sqlite.org/docs.html>
+ * Fernet - <https://cryptography.io/en/latest/fernet/>
+ * Bootstrap - <https://getbootstrap.com/docs/5.0/getting-started/introduction/>
+
+
+
+
+#### About CS50:
+CS50 is a openware course from Havard University and taught by David J. Malan
+
+Introduction to the intellectual enterprises of computer science and the art of programming. This course teaches students how to think algorithmically and solve problems efficiently. Topics include abstraction, algorithms, data structures, encapsulation, resource management, security, and software engineering. Languages include C, Python, and SQL plus students’ choice of: HTML, CSS, and JavaScript (for web development).
+
+Thank you for all CS50.
+
---
+++ app.py
@@ -0,0 +1,235 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from cryptography.fernet import Fernet
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+import datetime
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///database.db")
+
+# Encryption For Password Manager
+key = b'DCnnYi1vt1A3QxkMaa5lG17WHGMGjOvtfhdm_e2pBMU='
+fernet = Fernet(key)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
+@app.route("/")
+@login_required
+def main():
+    return render_template("home.html")
+
+
+
+
+
+
+@app.route("/index")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    user_id = session["user_id"]
+    tasks = db.execute("SElECT * FROM tasks WHERE user_id = ?", user_id)
+    return render_template("index.html",tasks = tasks)
+
+
+@app.route("/clipboard", methods=["GET","POST"])
+@login_required
+def Clipboard():
+    """Show Calendar"""
+    if request.method == "GET":
+        return render_template("clipboard.html")
+
+
+    user_id = session["user_id"]
+    note = request.form.get("note")
+    title = request.form.get("title")
+
+    db.execute("INSERT INTO clipboard (user_id,note,date_added,title) VALUES(?,?,?,?)",user_id,note,datetime.datetime.now(),title)
+    flash("Note Added!")
+    return redirect("/clipboard")
+
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/tasks", methods=["GET", "POST"])
+@login_required
+def tasks():
+    """Get List of all the tasks"""
+    if request.method == "GET":
+        return render_template("tasks.html")
+
+    user_id = session["user_id"]
+    task = request.form.get("task")
+    deadline = request.form.get("deadline")
+    db.execute("INSERT INTO tasks (user_id,task,deadline,completed) VALUES(?,?,?,?)",user_id,task,deadline,0)
+    flash("Task Added Successfully!!")
+    return redirect("/")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_pass = request.form.get("confirmation")
+        if not username:
+            return apology("A username Must be Provided")
+        if not password:
+            return apology("A Password Must be Provided")
+        if not confirm_pass:
+            return apology("A Confirmation Password Must be Provided")
+        if password != confirm_pass:
+            return apology("Different Passwords Entered")
+        else:
+            hash = generate_password_hash(password)
+            try:
+                db.execute("INSERT INTO users (username,hash) VALUES(?,?);", username, hash)
+                return redirect("/login")
+            except:
+                return apology("Username Already Exists")
+
+
+
+@app.route("/change_password", methods=["GET", "POST"])
+@login_required
+def change():
+    if request.method == "GET":
+        return render_template("change_password.html")
+
+    password = request.form.get("password")
+    confirm_pass = request.form.get("confirm_password")
+
+    if (password != confirm_pass):
+        return apology("Passwords Do Not Match!")
+
+    hash = generate_password_hash(password)
+    user_id = session["user_id"]
+    db.execute("UPDATE users SET hash = ? WHERE id=?", hash, user_id)
+    flash("Password Changed!")
+    return redirect("/login")
+
+
+
+@app.route("/notes",methods=["GET","POST"])
+@login_required
+def notes():
+    user_id = session["user_id"]
+    notes = db.execute("SELECT * FROM clipboard WHERE user_id=?",user_id)
+    return render_template("notes.html",notes = notes)
+
+
+
+@app.route("/password_manager",methods=["GET","POST"])
+@login_required
+def manager():
+    if request.method == "GET":
+        return render_template("password_manager.html")
+
+    user_id = session["user_id"]
+    master = request.form.get("master")
+    rows = db.execute("SELECT hash FROM users WHERE id=?",user_id)
+    if len(rows) != 1 or not check_password_hash(rows[0]["hash"], master):
+        return apology("Wrong Master Password!")
+
+    passwords = db.execute("SELECT * FROM passwords WHERE user_id=?",user_id)
+    real_pass = {}
+    for row in passwords:
+        pass1 = fernet.decrypt(row["password"]).decode()
+        real_pass[row["title"]] = pass1
+
+    print(real_pass)
+    return render_template("passwords.html",real_pass=real_pass)
+
+@app.route("/add_pass",methods=["GET","POST"])
+@login_required
+def add_pass():
+    if request.method == "GET":
+        return render_template("add_pass.html")
+
+
+    user_id = session["user_id"]
+    title = request.form.get("title")
+    new_password = request.form.get("password")
+    encpass = fernet.encrypt(new_password.encode())
+    db.execute("INSERT INTO passwords (user_id,password,title,date) VALUES(?,?,?,?)",user_id,encpass,title,datetime.datetime.now())
+    flash("Password Added!")
+    return redirect("/password_manager")
+
+
